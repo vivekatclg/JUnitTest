488ffdcca5b367e2c5afb547cbaa3e746eb2dd8a
vivekatclg <vivekatclg@gmail.com>
2014-04-15 00:56:44 -0400

    Update TestHelloWorld.java

--- JenkinsTest/src/TestHelloWorld.java
+++ JenkinsTest/src/TestHelloWorld.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 public class TestHelloWorld {
 
 /** A test that always fails. **/
-	@Test public void helloTest() {
+	@Test 
+	public void helloTest() {
 		HelloWorld hw = new HelloWorld();
 		String output = hw.getM();
         	Assert.assertEquals("Hello, World!", output);
